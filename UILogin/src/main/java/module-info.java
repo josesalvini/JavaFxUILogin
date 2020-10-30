3e53e1ba0b4046c49bf241cdbfa7c6672ed0fc4c
josesalvini <josesalvini@gmail.com>
2020-10-30 10:47:16 -0300

    jocho - Init project

---
+++ UILogin/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.ps.uilogin {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.ps.uilogin to javafx.fxml;
+    exports com.ps.uilogin;
+}
+*/
